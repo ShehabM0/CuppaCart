8ca16a8084f449bf688fd97554aa6bacaed33b45
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-04-10 23:37:03 +0200

    User Validation & New UI

--- Screens/Sign-up.js
+++ Screens/Sign-up.js
@@ -1,317 +1,168 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React,{useState} from 'react';
 import {
-    View,
-    Text,
-    Image,
-    TextInput,
-    ScrollView,
-    TouchableWithoutFeedback,
-    Keyboard,
-    StyleSheet,
-  } from "react-native";
-  import { AntDesign } from '@expo/vector-icons';
-  import React, { useState } from "react";
-  import { Fumi,Kaede,Hideo } from 'react-native-textinput-effects';
-  import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-  import CommonButton from "../Components/CommonButton";
-  export default function SignUp({navigation}) {
-    // const navigation = useNavigation();
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [secureTextEntry, setSecureTextEntry] = useState(true);
-    const [confsecureTextEntry, setconfSecureTextEntry] = useState(true);
+  View,
+  Text,
+  SafeAreaView,
+  Keyboard,
+  ScrollView,
+  Alert,
+} from 'react-native'; 
+
+import COLORS from '../Conts/Color';
+import Button from '../Components/Button';
+import Input from '../Components/Input';
+import Loader from '../Components/Loader';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase/config";
+import { sendEmailVerification } from "firebase/auth";
+import { register, getUserUId } from "../firebase/auth";
+import { addUser } from "../firebase/user";
+
+const RegistrationScreen = ({navigation}) => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [errors, setErrors] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
+
+  const validate = async () => {
+    Keyboard.dismiss();
+    let isValid = true;
   
+    if (!email) {
+      handleError('Please enter email', 'email');
+      isValid = false;
+    } else if (!email.match(/\S+@\S+\.\S+/)) {
+      handleError('Please enter a valid email', 'email');
+      isValid = false;
+    }
   
-    
-    return (
-      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            Keyboard.dismiss();
-          }}
-        >
-          <View style={{ flex: 1 }}>
-            <Text
-              style={{
-                marginTop: 50,
-                alignSelf: "center",
-                fontSize: 24,
-                fontWeight: "600",
-                color: "#000",
-              }}
-            >
-              Create New Account
-            </Text>
-            <Fumi
-      label={'Name'}
-      iconClass={FontAwesomeIcon}
-      iconName={'user'}
-      iconColor={'#000000'}
-      iconSize={20}
-      iconWidth={40}
-      inputPadding={20}
-      value={name}
-      onChangeText={setName}
-      style={{backgroundColor:"#D1D1D1" ,textColor:"#000000" , width:"98%",marginTop:15,marginLeft:5,borderRadius:10}}
-    />
+    if (!fullname) {
+      handleError('Please enter fullname', 'fullname');
+      isValid = false;
+    }
   
-  <Fumi
-      label={'Email'}
-      iconClass={FontAwesomeIcon}
-      iconName={'envelope-o'}
-      iconColor={'#000000'}
-      iconSize={20}
-      iconWidth={40}
-      inputPadding={20}
-      value={email}
-      keyboardType={"email-address"}
-      onChangeText={setEmail}
-      style={{backgroundColor:"#D1D1D1" ,textColor:"#000000" , width:"98%",marginTop:15,marginLeft:5,borderRadius:10}}
-    />
-   
-           <Fumi
-      label={'Phone Numer'}
-      iconClass={FontAwesomeIcon}
-      iconName={'mobile-phone'}
-      iconColor={'#000000'}
-      iconSize={20}
-      iconWidth={40}
-      inputPadding={20}
-      keyboardType={"number-pad"}
-      value={mobile}
-      onChangeText={setMobile}
-      style={{backgroundColor:"#D1D1D1" ,textColor:"#000000" , width:"98%",marginTop:15,marginLeft:5,borderRadius:10}}
-    />
-   <View>
-   <Fumi
-      label={'Password'}
-      iconClass={FontAwesomeIcon}
-      iconName={'eye'}
-      iconColor={'#000000'}
-      iconSize={20}
-      iconWidth={40}
-      inputPadding={20}
-      value={password}
-      onChangeText={
-        setPassword
+    if (!phone) {
+      handleError('Please enter phone number', 'phone');
+      isValid = false;
+    }
+  
+    if (!password) {
+      handleError('Please enter password', 'password');
+      isValid = false;
+    } else if (password.length < 5) {
+      handleError('Min password length of 5', 'password');
+      isValid = false;
+    }
+  
+    if (isValid) {
+      register(email, password)
+      .then(() => {
+        getUserUId().then((id) => {
+          // console.log(id);
+          addUser({
+            id: id,
+            email,
+            password,
+            fullname,
+            phone,
+            Role: "User",
+            image:
+              "https://64.media.tumblr.com/d82d24956974272dff1f745a004a43bf/tumblr_o51oavbMDx1ugpbmuo3_540.png",
+            cart: [],
+            favorite:[],
+            balance:0
+          });
+        });
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false);
+          navigation.navigate('SignIn');
+        }, 3000);
+      })
+      .catch((error)=>{
+        alert(error.message);
+      }) 
+       
       }
-      secureTextEntry={secureTextEntry}
-      style={{backgroundColor:"#D1D1D1" ,color:"#000000" , width:"98%",marginTop:15,marginLeft:5  , borderRadius:10}}
-    />
-    <AntDesign name="eyeo" size={24} color="black"   onPress={() => {
-              setSecureTextEntry(!secureTextEntry);
-              return false;
-            }} style={{marginLeft:"91%" ,marginTop:-45,paddingBottom:20}} />
-   </View>
+  };
   
-            <CommonButton
-              title={"Sign Up"}
-              textColor={"#fff"}
-            />
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: "800",
-                alignSelf: "center",
-                marginTop: 20,
-                marginBottom: 50,
-              }}
-              onPress={() => {
-                navigation.navigate("SignIn");
-              }}
-            >
-              Already have account?
-            </Text>
-          </View>
-        </TouchableWithoutFeedback>
+
+  const handleOnchange = (text, input) => {
+    setInputs(prevState => ({...prevState, [input]: text}));
+  };
+  const handleError = (error, input) => {
+    setErrors(prevState => ({...prevState, [input]: error}));
+  };
+  return (
+    <SafeAreaView style={{backgroundColor: COLORS.white, flex: 1}}>
+      <Loader visible={loading} />
+      <ScrollView
+        contentContainerStyle={{paddingTop: 50, paddingHorizontal: 20}}>
+        <Text style={{color: COLORS.black, fontSize: 40, fontWeight: 'bold'}}>
+          Register
+        </Text>
+        <Text style={{color: COLORS.grey, fontSize: 18, marginVertical: 10}}>
+          Enter Your Details to Register
+        </Text>
+        <View style={{marginVertical: 20}}>
+          <Input
+             value={email}
+             onChangeText={setEmail}
+            onFocus={() => handleError(null, email)}
+            iconName="email-outline"
+            label="Email"
+            placeholder="Enter your email address"
+            error={errors.email}
+          />
+
+          <Input
+            value={fullname}
+            onChangeText={setFullname}
+            onFocus={() => handleError(null, fullname)}
+            iconName="account-outline"
+            label="Full Name"
+            placeholder="Enter your full name"
+            error={errors.fullname}
+          />
+
+          <Input
+            keyboardType="numeric"
+            value={phone}
+            onChangeText={setPhone}
+            onFocus={() => handleError(null, phone)}
+            iconName="phone-outline"
+            label="Phone Number"
+            placeholder="Enter your phone no"
+            error={errors.phone}
+          />
+          <Input
+            value={password}
+            onChangeText={setPassword}
+            onFocus={() => handleError(null, password)}
+            iconName="lock-outline"
+            label="Password"
+            placeholder="Enter your password"
+            error={errors.password}
+            password
+          />
+          <Button title="Register" onPress={validate} />
+          <Text
+            onPress={() => navigation.navigate('SignIn')}
+            style={{
+              color: COLORS.black,
+              fontWeight: 'bold',
+              textAlign: 'center',
+              fontSize: 16,
+            }}>
+            Already have account ?Login
+          </Text>
+        </View>
       </ScrollView>
-    );
-  }
-  
-  // import { StatusBar } from "expo-status-bar";
-  // import {
-  //   StyleSheet,
-  //   Text,
-  //   View,
-  //   Button,
-  //   TextInput,
-  //   TouchableWithoutFeedback,
-  //   ImageBackground,
-  //   Keyboard,
-  //   KeyboardAvoidingView,
-  //   TouchableOpacity,
-  //   Icon,
-  // } from "react-native";
-  // import {createUserWithEmailAndPassword } from "firebase/auth";
-  // import  auth  from "./firebase";
-  // import { useState } from "react";
-  // const Register = ({ navigation }) => {
-  //   const [name, setName] = useState("");
-  //   const [phone, setPhone] = useState("");
-  //   const [email, setEmail] = useState("");
-  //   const [password, setPassword] = useState("");
-  //   const [confirmPassword, setConfirmPassword] = useState("");
-  //   const handleRegister = ()=>{
-  //     createUserWithEmailAndPassword(auth,email,password)
-  //     .then((userCredential) => {
-  //       console.log("ok")
-  //       const user = userCredential.user;
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code;
-  //       const errorMessage = error.message;
-  //       console.log(errorMessage);
-  
-  //     });
-  
-  //   };
-  
-  //   return (
-  //     <TouchableWithoutFeedback
-  //       onPress={() => {
-  //         Keyboard.dismiss();
-  //       }}
-  //     >
-  //       <View style={styles.container}>
-  //         <ImageBackground
-  //           //source={require("../img/peakpx.jpg")}
-  //           resizeMode="cover"
-  //           style={styles.image}
-  //         >
-  //           <Text style={styles.textedit}>Register</Text>
-  // {/*
-  //           <TextInput
-  //             style={styles.textInput}
-  //             placeholder="Name"
-  //             value={name}
-  //             onChangeText={(text) => setName(text)}
-  //           />
-  //           <TextInput
-  //             style={styles.textInput}
-  //             keyboardType="number-pad"
-  //             placeholder="Phone"
-  //             value={phone}
-  //             onChangeText={(Number) => setPhone(Number)}
-  //           /> */}
-  //           <TextInput
-  //             style={styles.textInput}
-  //             placeholder="Email Address"
-  //             value={email}
-  //             onChangeText={setEmail}
-  //           />
-  //           <TextInput
-  //             style={styles.textInput}
-  //             placeholder="Enter Password"
-  //             value={password}
-  //             onChangeText={setPassword}
-  //             secureTextEntry={true}
-  //           />
-  //           {/* <TextInput
-  //             style={styles.textInput}
-  //             placeholder="Confirm Password"
-  //             value={confirmPassword}
-  //             onChangeText={(text) => setConfirmPassword(text)}
-  //             secureTextEntry={true}
-  //           /> */}
-  //           <TouchableOpacity activeOpacity={1} style={styles.buttonContainer}
-  //           onPress={handleRegister}
-  //           >
-  //             <Text style={styles.txtP}>Register</Text>
-  //           </TouchableOpacity>
-  //           <TouchableOpacity>
-  //             <Text
-  //               style={styles.textedit2}
-  //               onPress={() => navigation.navigate("Login")}
-  //             >
-  //               Already a member? SignIn
-  //             </Text>
-  //           </TouchableOpacity>
-  //           <TouchableOpacity
-  //             activeOpacity={1}
-  //             style={styles.homebutton}
-  //             onPress={() => navigation.navigate("Home")}
-  //           >
-  //             <Text style={styles.txtP}>Go to Home</Text>
-  //           </TouchableOpacity>
-  
-  //           <StatusBar style="auto" />
-  //         </ImageBackground>
-  //       </View>
-  //     </TouchableWithoutFeedback>
-  //   );
-  // };
-  // export default Register;
-  
-  // const styles = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: "#fff",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //   },
-  //   container: {
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //   },
-  //   textedit: {
-  //     color: "#ffff",
-  //     fontSize: 30,
-  //     padding: 8,
-  //   },
-  //   textedit2: {
-  //     color: "#ffff",
-  //     fontSize: 15,
-  //     padding: 8,
-  //   },
-  
-  //   buttonContainer: {
-  //     alignItems: "center",
-  //     backgroundColor: "#0047AB",
-  //     width: "70%",
-  //     padding: 10,
-  //     marginTop: 10,
-  //     borderRadius: 12,
-  //   },
-  
-  //   homebutton: {
-  //     alignItems: "center",
-  //     backgroundColor: "#0047AB",
-  //     width: "50%",
-  //     padding: 10,
-  //     marginTop: 90,
-  //     borderRadius: 15,
-  //     position: "absolute",
-  //     bottom: 30,
-  //   },
-  
-  //   txtP: {
-  //     color: "#fff",
-  //     fontSize: 15,
-  //   },
-  //   buttonContainer2: {
-  //     marginLeft: 200,
-  //     marginRight: 30,
-  //     marginTop: -50,
-  //     flexDirection: "row",
-  //   },
-  
-  //   textInput: {
-  //     marginTop: 15,
-  //     borderWidth: 3,
-  //     borderColor: "#0000",
-  //     backgroundColor: "#ffff",
-  //     color: "#120438",
-  //     borderRadius: 15,
-  //     width: "90%",
-  //     padding: 13,
-  //   },
-  //   image: {
-  //     width: "100%",
-  //     height: "100%",
-  //     margin: 20,
-  //     justifyContent: "center",
-  //     alignItems: "center",
-  //   },
-  // });
-  
+    </SafeAreaView>
+  );
+};
+
+export default RegistrationScreen;
--- Screens/StartupScreen.js
+++ Screens/StartupScreen.js
@@ -1,78 +1,83 @@
 import {
-    ImageBackground,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-  } from "react-native";
-  import React from "react";
-  const WelcomeScreen = ({navigation}) => {
-    return (
-      <ImageBackground
-        style={{ flex: 1 }}
-        source={require("../assets/coffee5.jpg")}
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  StatusBar,
+  Dimensions, Image, 
+} from "react-native";
+import React from "react";
+const Home = ({ navigation }) => {
+  return (
+      
+    <ImageBackground
+      style={{flex: 1}}
+      source={require("../assets/coffee.jpg")}
+    >
+       
+      <View style={{ flex: 1, backgroundColor: "black", opacity: 0.2 }} />
+      <View
+        style={{
+          position: "absolute",
+          height: "100%",
+          zIndex: 2,
+          width: "100%",
+          justifyContent: "flex-end",
+          paddingHorizontal: 20,
+          paddingBottom: 50,
+        }}
       >
-        <View style={{ flex: 1, backgroundColor: "black", opacity: 0.2 }} />
-        <View
-          style={{
-            position: "absolute",
-            height: "100%",
-            zIndex: 2,
-            width: "100%",
-            justifyContent: "flex-end",
-            paddingHorizontal: 20,
-            paddingBottom: 50,
-          }}
-        >
-          <View>
+        <View>
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "800",
+              fontSize: 45,
+              
+            }}
+          >
+            Coffee so good, your taste buds will love it.
+          </Text>
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "300",
+              fontSize: 17,
+              
+              
+            }}
+          >
+           The best grain, the finest roast & the powerful flavor
+          </Text>
+          <TouchableOpacity
+            style={{
+              padding: 20,
+              backgroundColor: "white",
+              borderRadius: 20,
+              alignItems: "center",
+              marginTop: 30,
+            }}
+            onPress={() => {
+              navigation.navigate("SignIn");
+            }}
+          >
             <Text
               style={{
-                color: "white",
-                fontWeight: "800",
-                fontSize:45,
-                textTransform: "capitalize",
+                color: "black",
+                fontSize: 20,
+                fontWeight: "700",
               }}
             >
-              Let your favorite Coffee find you
+              Get Started
             </Text>
-            <Text
-              style={{
-                color: "white",
-                fontWeight: "600",
-                fontSize: 17,
-              }}
-            >
-              Dolore reprehenderit id ea eu voluptate deserunt occaecat occaecat.
-            </Text>
-            <TouchableOpacity
-              style={{
-                padding: 20,
-                backgroundColor:"white",
-                borderRadius: 20,
-                alignItems: "center",
-                marginTop: 30,
-              }}
-              onPress={() => {
-                navigation.navigate("SignIn");
-              }}
-            >
-              <Text
-                style={{
-                  color: "black",
-                  fontSize: 20,
-                  fontWeight: "700",
-                }}
-              >
-                Explorer Now
-              </Text>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
         </View>
-      </ImageBackground>
-    );
-  };
-  
-  export default WelcomeScreen;
-  
-  const styles = StyleSheet.create({});
-  
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
