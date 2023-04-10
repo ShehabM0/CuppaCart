fd37f55497f91d38127e58946f46e84b87295418
Muhammed Walied <mw2242001@gmail.com>
2023-04-10 22:47:59 +0200

    Modifyed App.js

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { useState, useEffect }from 'react';
 
+import MainNav from './Routes/MainNav';
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <View style={styles.textCont}>
-        <Text style={styles.text}>If you read this then it works!!</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <MainNav/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textCont: {
-    backgroundColor: 'black',
-    borderRadius: 5,
-    padding: 20,
-  },
-  text: {
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
