9f8ff9e1271b123b3a3df966c5a7375138a69943
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-04-10 23:30:22 +0200

    Colors of the project

---
+++ Conts/Color.js
@@ -0,0 +1,14 @@
+const COLORS = {
+    white: '#fff',
+    black: '#1C0A00',
+    blue: '#5D5FEE',
+    grey: '#BABBC3',
+    light: '#F3F4FB',
+    darkBlue: '#7978B5',
+    red: 'red',
+    darkOrange:"#361500",
+    darkModOrange:"#865439",
+    lightBlue:"ADD8E6"
+  };
+  export default COLORS;
+  
