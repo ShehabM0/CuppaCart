05eda945ebd144612c70a703b0fb31b6154c1a59
mokhtar <mohamedmokhtarm3@gmail.com>
2023-04-12 00:08:16 +0200

    modify ProfileScreen and fetch data from firestore.

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,25 +1,223 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import React,{useState,useEffect} from 'react';
+const backImage = require("../assets/adaptive-icon.png");
+import { Ionicons } from '@expo/vector-icons';
+
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from 'react-native-paper';
+import { StyleSheet, currentUser, SafeAreaView, TouchableOpacity, View,ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import auth from "../firebase/config";
+import {
+  getUserUId,
+  addUser,
+  deleteUser,
+  editUser,
+  getUserById,
+  getUserByName,
+  getUsers,
+  subscribeUser,
+} 
+from "../firebase/user"
+// import 'firebase/firestore';
+// import {getUsers}from"../firebase/user";
 
 const ProfileScreen = ({navigation}) => {
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
-     
-        <TouchableOpacity  onPress={() => {
-          navigation.navigate("SettingsTab");
-        
-       }}>
-        <Text>Go</Text>
-        </TouchableOpacity>
 
-    </View>
-  )
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setname] = useState("");
+  const [image, setimage] = useState(null);
+  const [proimage, setproimage] = useState(null);
+  const [phone, setPhone] = useState("");
+  const [role, setRole] = useState("");
+  const [balance, setBalance] = useState();
+
+  
+  
+
+  useEffect(() => {
+    getUserUId().then((id) => {
+       //console.log(id);
+      getUserById(id).then((user) => {
+       // console.log(user);
+        setEmail(user[0].email);
+        setPassword(user[0].password);
+        setname(user[0].fullname);
+        setPhone(user[0].phone);
+        setimage(user[0].image);
+        setRole(user[0].Role);
+        setBalance(user[0].balance)
+      });
+    });
+  }, []);
+
+console.log(fullname);
+
+
+
+
+  return (
+    <ScrollView
+    style={{
+      padding: 22,
+      backgroundColor:"#2E333E"
+    }}
+  > 
+    <SafeAreaView style={styles.container}>
+
+
+
+      <View style={styles.userInfoSection}>
+        <View style={{ flexDirection: 'row', marginTop: 36 }}>
+          <Avatar.Image
+            source={{uri: image}} size={60}/>
+
+          <View style={{ marginLeft: 5}}>
+            <Title style={[styles.title, {
+              marginTop: 20,
+              marginBottom: 5,
+              color:'white',fontSize:25
+            }]}>{fullname}</Title>
+
+
+
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Icon name="map-marker-radius" color="#777777" size={20} />
+          <Text style={{ color: "#fff", marginLeft: 20 }}>Cairo,Egypt</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="phone" color="#777777" size={20} />
+          <Text style={{ color: "#fff", marginLeft: 20 }}>{phone}</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="email" color="#777777" size={20} />
+          <Text style={{ color: "#fff", marginLeft: 20 }}>{email}</Text>
+        </View>
+      </View>
+
+      <View style={styles.infoBoxWrapper}>
+        <View style={[styles.infoBox, {
+          borderRightColor: '#dddddd',
+          borderRightWidth: 1
+        }]}>
+          <Title style={{ color:'white' }} >-</Title>
+          <Caption style={{ color:'white',fontSize:15 }} >Bonus</Caption>
+        </View>
+        <View  style={[styles.infoBox, {
+          borderRightColor: '#dddddd',
+          borderRightWidth: 1
+        }]}>
+          <Title style={{ color:'white' }} >-</Title>
+          <Caption style={{ color:'white',fontSize:15 }} >Orders</Caption>
+          
+        </View>
+
+        <View style={styles.infoBox}>
+          <Title style={{ color:'white' }} >{balance}</Title>
+          <Caption style={{ color:'white',fontSize:15 }} >Balance</Caption>
+        </View>
+        
+      </View>
+
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={() => { }}>
+          <View style={styles.menuItem}>
+            <Icon name="heart-outline" color="#964B00" size={25} />
+            <Text style={styles.menuItemText}>Your Favorites</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => { }}>
+          <View style={styles.menuItem}>
+            <Icon name="credit-card" color="#964B00" size={25} />
+            <Text style={styles.menuItemText}>Payment</Text>
+          </View>
+        </TouchableRipple>
+
+
+
+        <TouchableRipple onPress={() => { }}>
+          <View style={styles.menuItem}>
+            <Icon name="account-check-outline" color="#964B00" size={25} />
+            <Text style={styles.menuItemText}>Support</Text>
+          </View>
+        </TouchableRipple>
+
+        <TouchableRipple onPress={() => {
+          navigation.navigate("SettingsTab")}}>
+          <View style={styles.menuItem}>
+          <Ionicons name="settings-outline" size={25} color="#964B00" />
+            <Text style={styles.menuItemText}>Settings</Text>
+          </View>
+        </TouchableRipple>
+      </View>
+
+
+    </SafeAreaView>
+    </ScrollView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: '500',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+    borderTopColor: '#dddddd',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    height: 100,
+  },
+  infoBox: {
+    width: '35%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color:'#fff',
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: '#fff',
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
 
 export default ProfileScreen
