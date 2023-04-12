75521aa9e2bf0952904144cd73760c31d8c34e7a
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-04-12 17:24:14 +0200

    handled validations

--- Screens/Sign-in .js
+++ Screens/Sign-in .js
@@ -1,108 +1,128 @@
-import React,{useState} from 'react';
-import {View, Text, SafeAreaView, Keyboard, Alert} from 'react-native';
-import COLORS from '../';
-import Button from '../Components/Button';
-import Input from '../Components/Input';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Loader from '../Components/Loader';
+import React, { useState,useEffect   } from "react";
+import { View, Text, SafeAreaView, Keyboard, Alert } from "react-native";
+import COLORS from "../";
+import Button from "../Components/Button";
+import Input from "../Components/Input";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Loader from "../Components/Loader";
 import auth from "../firebase/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { addUser, getUserById, getUserUId } from "../firebase/user";
-import { login, getUserToken,logout } from "../firebase/auth";
+import { login, getUserToken, logout } from "../firebase/auth";
 
-const LoginScreen = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errors, setErrors] = React.useState({});
   const [loading, setLoading] = React.useState(false);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, [navigation]);
 
   const validate = async () => {
     Keyboard.dismiss();
     if (!email) {
-      handleError('Please enter email', 'email');
+      handleError("Please enter email", "email");
       return;
     }
     if (!password) {
-      handleError('Please enter password', 'password');
+      handleError("Please enter password", "password");
       return;
     }
-      login(email, password)
-        .then(() => {
-          getUserUId().then((id) => {
-            getUserById(id).then((user) => {
-              if (user[0].Role === "Admin") {
-                navigation.navigate("Admin");
-              } else {
-                navigation.navigate("TabsNav");
-              }
-            });
+
+    login(email, password)
+      .then(() => {
+        getUserUId().then((id) => {
+          getUserById(id).then((user) => {
+            if (user[0].Role === "Admin") {
+              navigation.navigate("Admin");
+            } else {
+              navigation.navigate("TabsNav");
+            }
           });
-          setLoading(true);
-        })
-        .catch((e) => {
-          alert("invalid email or password");
-          console.log(e.message);
         });
-    
+        setLoading(true);
+      })
+      .catch((e) => {
+        alert("invalid email or password");
+        console.log(e.message);
+      });
   };
-  
+
   const handleOnchange = (text, input) => {
-    setInputs(prevState => ({...prevState, [input]: text}));
+    if (input === "email") {
+      setEmail(text);
+      handleError(null, input);
+    } else if (input === "password") {
+      setPassword(text);
+      handleError(null, input);
+    }
   };
 
   const handleError = (error, input) => {
-    setErrors(prevState => ({...prevState, [input]: error}));
+    setErrors((prevState) => ({ ...prevState, [input]: error }));
   };
   return (
-    <SafeAreaView style={{backgroundColor: COLORS.white, flex: 1}}>
+    <SafeAreaView style={{ backgroundColor: COLORS.black, flex: 1 }}>
       <Loader visible={loading} />
-      <View style={{paddingTop: 50, paddingHorizontal: 20}}>
-        <Text style={{color: COLORS.black, fontSize: 40, fontWeight: 'bold'}}>
+      <View style={{ paddingTop: 50, paddingHorizontal: 20 }}>
+        <Text style={{ color: COLORS.black, fontSize: 40, fontWeight: "bold" }}>
           Log In
         </Text>
-        <Text style={{color: COLORS.grey, fontSize: 18, marginVertical: 10}}>
+        <Text style={{ color: COLORS.grey, fontSize: 18, marginVertical: 10 }}>
           Enter Your Details to Login
         </Text>
-        <View style={{marginVertical: 20}}>
+        <View style={{ marginVertical: 20 }}>
           <Input
-            onChangeText={setEmail}
+            onChangeText={(text) => handleOnchange(text, "email")}
             value={email}
-            onFocus={() => handleError(null, email)}
+            onFocus={() => handleError(null, "email")}
             iconName="email-outline"
             label="Email"
             placeholder="Enter your email address"
+            keyboardType={"email-address"}
             error={errors.email}
+
           />
           <Input
-            onChangeText={setPassword}
+            onChangeText={(text) => handleOnchange(text, "password")}
             value={password}
-            onFocus={() => handleError(null, password)}
+            onFocus={() => handleError(null, "password")}
             iconName="lock-outline"
             label="Password"
             placeholder="Enter your password"
+            
             error={errors.password}
             password
           />
+
           <Button title="Log In" onPress={validate} />
           <Text
-            onPress={() => navigation.navigate('SignUp')}
+            onPress={() => navigation.navigate("SignUp")}
             style={{
               color: COLORS.black,
-              fontWeight: 'bold',
-              textAlign: 'center',
+              fontWeight: "bold",
+              textAlign: "center",
               fontSize: 16,
-            }}>
+            }}
+          >
             Don't have account? Register
           </Text>
           <Text
-            onPress={() => navigation.navigate('ForgetPassword')}
+            onPress={() => navigation.navigate("ForgetPassword")}
             style={{
               color: COLORS.black,
-              fontWeight: 'bold',
-              textAlign: 'center',
+              fontWeight: "bold",
+              textAlign: "center",
               fontSize: 16,
-              padding:13
-            }}>
+              padding: 13,
+            }}
+          >
             Forgot password?
           </Text>
         </View>
--- Screens/StartupScreen.js
+++ Screens/StartupScreen.js
@@ -5,74 +5,63 @@ import {
   TouchableOpacity,
   View,
   StatusBar,
-  Dimensions, Image, 
+  Dimensions,
+  Image,
 } from "react-native";
+import Constants from "expo-constants";
 import React from "react";
+import TextAnimator from "../Components/TextAnimator";
+
+
 const Home = ({ navigation }) => {
+
+ 
   return (
-      
     <ImageBackground
-      style={{flex: 1}}
+      style={{ flex: 1 }}
       source={require("../assets/coffee.jpg")}
     >
-       
-      <View style={{ flex: 1, backgroundColor: "black", opacity: 0.2 }} />
-      <View
-        style={{
-          position: "absolute",
-          height: "100%",
-          zIndex: 2,
-          width: "100%",
-          justifyContent: "flex-end",
-          paddingHorizontal: 20,
-          paddingBottom: 50,
-        }}
-      >
-        <View>
+      <View style={styles.container} />
+      <View style={styles.textHead}>
+        <TextAnimator
+          content="️Coffee so good, your taste buds will love it."
+          textStyle={styles.textHeadStyle}
+          style={styles.textHeadStyle}
+          duration={800}
+         
+          
+        />
+
+        <TextAnimator
+          content="️The best grain, the finest roast & the powerful flavor"
+          textStyle={styles.textFootStyle}
+          style={styles.textFootStyle}
+          duration={800}
+         
+        />
+
+        <TouchableOpacity
+          style={{
+            padding: 20,
+            backgroundColor: "white",
+            borderRadius: 20,
+            alignItems: "center",
+            marginTop: 30,
+          }}
+          onPress={() => {
+            navigation.navigate("SignIn");
+          }}
+        >
           <Text
             style={{
-              color: "white",
-              fontWeight: "800",
-              fontSize: 45,
-              
+              color: "black",
+              fontSize: 20,
+              fontWeight: "700",
             }}
           >
-            Coffee so good, your taste buds will love it.
+            Get Started
           </Text>
-          <Text
-            style={{
-              color: "white",
-              fontWeight: "300",
-              fontSize: 17,
-              
-              
-            }}
-          >
-           The best grain, the finest roast & the powerful flavor
-          </Text>
-          <TouchableOpacity
-            style={{
-              padding: 20,
-              backgroundColor: "white",
-              borderRadius: 20,
-              alignItems: "center",
-              marginTop: 30,
-            }}
-            onPress={() => {
-              navigation.navigate("SignIn");
-            }}
-          >
-            <Text
-              style={{
-                color: "black",
-                fontSize: 20,
-                fontWeight: "700",
-              }}
-            >
-              Get Started
-            </Text>
-          </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -80,4 +69,24 @@ const Home = ({ navigation }) => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    opacity: 0.2,
+  },
+
+  containerStyle: {},
+  textHead: {
+    position: "absolute",
+    height: "100%",
+    zIndex: 2,
+    width: "100%",
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+
+  textHeadStyle: { color: "white", fontWeight: "800", fontSize: 45,  },
+  textFootStyle: { color: "white", fontWeight: "300", fontSize: 17 },
+});
