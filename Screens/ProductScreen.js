7d0a9ced85ea190caa99e99a9bcefdbde23162e9
ShehabM0 <shehabmohamed2357@gmail.com>
2023-04-13 00:22:42 +0200

    edited some styles

--- Screens/ProductScreen.js
+++ Screens/ProductScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { ScrollView, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-
+import { Platform, StatusBar, ScrollView, Image, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View, SafeAreaView } from 'react-native';
 import COLORS from '../Conts/Color'
 
 export default function ProductScreen() {
@@ -25,93 +24,102 @@ export default function ProductScreen() {
   }
 
   return (
-      <ScrollView>
-        <View style={styles.imgbgLayout}/>
-        <View style={styles.imgContCont}>
-          <View style={styles.imgCont}>
+      <SafeAreaView style={{paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 2 : 0,}}>
+        <ScrollView>
+          <View style={styles.imgbgLayout}/>
+          <View style={styles.imgTitlePriceCont}>
             <Image 
               style={styles.img}
-              source={require('../assets/nathan-dumlao-tA90pRfL2gM-unsplash.jpg')}>
+              source={require('../assets/nathan-dumlao-3.jpg')}>
             </Image>
-          </View>
-        </View>
-        <View style={styles.titlepriceCont}>
-          <View style={styles.title}>
-            <Text style={styles.titlepriceTxt}>
-              Product Title, could be Longer and longer
-            </Text>
-          </View>
-
-          <View style={styles.priceCont}>
-            <Text style={styles.titlepriceTxt}>
-              <Image 
-                style={styles.downArrow} 
-                source={require('../assets/dollar_sign.png')}
-              /> {price}
-            </Text>
-
-            <Text style={styles.titlepriceTxt}>
-              <Image 
-                style={styles.downArrow} 
-                source={require('../assets/coin.png')}
-              /> {coin}
-            </Text>
-            
-          </View>
-        </View>
-
-        <View style={styles.cont}>
-          <TouchableWithoutFeedback onPress={() => setArrow(!arrow)}>
-              <View style={styles.descCont}>
-                <Text style={styles.descTitle}>Description</Text>
-                { 
-                  arrow &&
-                  <Image 
-                    style={styles.downArrow} 
-                    source={require('../assets/arrow_down_icon.png')}
-                  />
-                } 
-                { 
-                  !arrow && 
-                  <Image 
-                    style={styles.downArrow} 
-                    source={require('../assets/arrow_up_icon.png')}
-                  />
-                }
+            <View style={styles.titlePriceCont}>
+              <View style={styles.title}>
+                <Text style={styles.titlepriceTxt}>
+                  Cappuccino
+                </Text>
+                <Text>
+                  <Image
+                    style={styles.imgStar}
+                    source={require('../assets/star.png')}>
+                  </Image>
+                  <Text style={styles.revRate}> 4.8</Text> 
+                  <Text style={styles.revNumber}> (1,254)</Text>
+                </Text>
               </View>
-          </TouchableWithoutFeedback>
-          { 
-            !arrow && 
-            <View>
-              <Text style={styles.desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-            </View>
-          }
-        </View>
 
-        <View style={styles.sizesCont}>
-          <TouchableOpacity style={styles.size} onPress={setSmall}>
-            <Text style={styles.sizeTxt}>Small</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.size} onPress={setMedium}>
-            <Text style={styles.sizeTxt}>Medium</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.size} onPress={setLarge}>
-            <Text style={styles.sizeTxt}>Large</Text>
-          </TouchableOpacity>
-        </View>
+              <View style={styles.priceCont}>
+                <Text style={styles.titlepriceTxt}>
+                  <Image 
+                    style={styles.dollarImg} 
+                    source={require('../assets/blue_dollar_sign.png')}
+                  /> {price}
+                </Text>
 
-        <View style={styles.cartContCont}>
-          <TouchableOpacity style={styles.cart}>
-            <View style={styles.cartCont}>
-              <Text style={styles.cartTxt}>Add To Cart</Text>
-              <Image 
-                style={styles.cartImg}
-                source={require('../assets/cart.png')}
-              />
+                <Text style={styles.titlepriceTxt}>
+                  <Image 
+                    style={styles.coinImg} 
+                    source={require('../assets/coin.png')}
+                  />  {coin}
+                </Text>
+              </View>
             </View>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+          </View>
+
+
+          <View style={styles.cont}>
+            <TouchableWithoutFeedback onPress={() => setArrow(!arrow)}>
+                <View style={styles.descCont}>
+                  <Text style={styles.descTitle}>Description</Text>
+                  { 
+                    arrow &&
+                    <Image 
+                      style={styles.downArrow} 
+                      source={require('../assets/arrow_down_icon.png')}
+                    />
+                  } 
+                  { 
+                    !arrow && 
+                    <Image 
+                      style={styles.downArrow} 
+                      source={require('../assets/arrow_up_icon.png')}
+                    />
+                  }
+                </View>
+            </TouchableWithoutFeedback>
+            { 
+              !arrow && 
+              <View>
+                <Text style={styles.desc}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+              </View>
+            }
+          </View>
+
+
+          <View style={styles.sizesCont}>
+            <TouchableOpacity style={styles.size} onPress={setSmall}>
+              <Text style={styles.sizeTxt}>Small</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.size} onPress={setMedium}>
+              <Text style={styles.sizeTxt}>Medium</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.size} onPress={setLarge}>
+              <Text style={styles.sizeTxt}>Large</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.cartContCont}>
+            <TouchableOpacity style={styles.cart}>
+              <View style={styles.cartCont}>
+                <Text style={styles.cartTxt}>Add To Cart</Text>
+                <Image 
+                  style={styles.cartImg}
+                  source={require('../assets/cart.png')}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
   );
 }
 
@@ -120,30 +128,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     margin: 10,
   },
-  descCont: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: COLORS.white,
-    borderRadius: 20,
-    padding: 5
-  },
-  descTitle: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: COLORS.black,
-    paddingLeft: 10
-  },
-  desc: {
-    fontWeight: 'bold',
-    padding: 10,
-    color: COLORS.white,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
-    borderRadius: 20
-  },
-  downArrow: {
-    width: 30,
-    height: 30,
-  },
 
   imgbg: {
     flex: 1,
@@ -151,87 +135,138 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   imgbgLayout: {
-    backgroundColor: COLORS.bisque,
+    backgroundColor: COLORS.darkBlue1,
     position: 'absolute',
     width: '100%',
     height: '100%',
   },
-  imgContCont: {
+  imgTitlePriceCont: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 50,
-    marginBottom: 30,
-  },
-  imgCont: {
-    shadowColor: '#52006A',  
-    elevation: 20,  
+    backgroundColor: 'red'
   },
   img: {
-    width: '80%',
+    width: '100%',
     height: undefined,
     aspectRatio: 1,
-    borderRadius: 15,
   },
-  titlepriceCont: {
+
+  titlePriceCont: {
+    position: 'absolute',
+    bottom: 0,
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
     padding: 10,
     fontSize: 22,
     fontWeight: 'bold',
+    backgroundColor: 'rgba(0,0,0,0.45)',
+    borderRadius: 20,
   },
   title: {
     width: '70%',
-    paddingLeft: 20
+    paddingLeft: 20,
   },
   priceCont: {
     width: '40%',
   },
   titlepriceTxt: {
-    color: COLORS.black,
-    fontSize: 20,
+    color: COLORS.white,
+    fontSize: 18,
     fontWeight: 'bold',
+  },
+  dollarImg: {
+    width: 30,
+    height: 30,
+  },
+  coinImg: {
+    width: 22,
+    height: 22,
+  },
+  imgStar: {
+    marginTop: 5,
+    width: 25,
+    height: 25,
+  },
+  revRate: {
+    color: COLORS.white,
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  revNumber: {
+    color: '#A9A9A9',
+  },
+
+  descCont: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: COLORS.black,
+    borderRadius: 20,
+    padding: 5,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: COLORS.lightOrange,
+  },
+  descTitle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: COLORS.white,
+    paddingLeft: 10,
+  },
+  desc: {
+    fontWeight: 'bold',
+    padding: 10,
+    color: COLORS.white,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    borderRadius: 20,
+  },
+  downArrow: {
+    width: 30,
+    height: 30,
   },
 
   sizesCont: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginVertical: 20,
+    marginVertical: 10,
   },
   size: {
     backgroundColor: COLORS.black,
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
+    borderWidth:  1,
+    borderColor: COLORS.lightOrange,
   },
   sizeTxt: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: COLORS.white
+    color: COLORS.white,
   },
 
 
   cartContCont: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 20
+    marginVertical: 20,
   },
-
   cartCont: {
     flexDirection: 'row',
-    backgroundColor: '#00FF00',
+    backgroundColor: COLORS.black,
     borderRadius: 15,
     paddingHorizontal: 50,
-    paddingVertical: 10
+    paddingVertical: 10,
+    borderWidth:  1,
+    borderColor: COLORS.lightOrange,
   },
   cartTxt: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: COLORS.white
+    color: COLORS.white,
   },
   cartImg: {
     width: 30,
     height: 30,
-    marginLeft: 20
-  }
+    marginLeft: 20,
+  },
 });
 
