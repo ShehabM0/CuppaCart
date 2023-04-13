fdfd15eddf73161fa4eb5244c098b6285f0919de
Muhammed Walied <mw2242001@gmail.com>
2023-04-13 04:13:12 +0200

    Display Product in Home Page

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -9,116 +9,117 @@ import {
   Image,
   Dimensions,
   ImageBackground,
-  FlatList
+  FlatList,
+  TextInput,
 } from "react-native";
+import auth from "../firebase/config";
+import { SimpleLineIcons } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  getUserUId,
+  addUser,
+  deleteUser,
+  editUser,
+  getUserById,
+  getUserByName,
+  getUsers,
+  subscribeUser,
+} from "../firebase/user";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getProductByName,
+  getProducts,
+  subscribeProduct,
+} from "../firebase/products";
+import ProductCard from "../Components/productCard";
+import { logout } from "../firebase/auth";
 
-
-
- const { width } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const d = Dimensions.get("window");
-export default function ProfileScreen({navigation}) {
-//  const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [name, setname] = useState("");
-//   const [image, setimage] = useState(null);
-//   const [proimage, setproimage] = useState(null);
-//   const [mobile, setMobile] = useState("");
-//   const [role, setRole] = useState("");
-//   const [products, setProducts] = useState([]);
+export default function ProfileScreen({ navigation }) {
+  const [fullname, setfullname] = useState("");
+  const [image, setimage] = useState(null);
+  const [role, setRole] = useState("");
+  const [products, setProducts] = useState([]);
 
-//   const getProductHandle = async () => {
-//     const arr = await getProducts();
-//   };
+  const getProductHandle = async () => {
+    const arr = await getProducts();
+    setProducts(arr);
+  };
 
-//   useEffect(() => {
-//     getProductHandle();
-//   }, []);
-//   const ss=()=>{
-  
-//     logout(auth).then(()=>{
-//       console.log("sign out done");
-//         navigation.navigate('Sign In')
-//     })
-//   }
-//   useEffect(() => {
-//     getUserUId().then((id) => {
-//        //console.log(id);
-//       getUserById(id).then((user) => {
-//        // console.log(user);
-//         setEmail(user[0].email);
-//         setPassword(user[0].password);
-//         setname(user[0].name);
-//         setMobile(user[0].mobile);
-//         setimage(user[0].image);
-//         setRole(user[0].Role);
-//       });
-//     });
-//   }, []);
+  useEffect(() => {
+    getProductHandle();
+  }, []);
+  const ss = () => {
+    logout(auth).then(() => {
+      console.log("sign out done");
+      navigation.navigate("SignIn");
+    });
+  };
+  useEffect(() => {
+    getUserUId().then((id) => {
+      getUserById(id).then((user) => {
+        setfullname(user[0].fullname);
+        setimage(user[0].image);
+        setRole(user[0].Role);
+      });
+    });
+  }, []);
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <ScrollView
-    style={{
-      padding: 22,
-      backgroundColor:"#2E333E"
-    }}
-  > 
-  
     <View
-    style={{
-      flexDirection: "row",
-      justifyContent: "space-between",
-      
-
-    }}
-  >
-    <TouchableOpacity
       style={{
-        borderRadius: 25,
-        overflow: "hidden",
-        width: 40,
-        height: 40,
+        padding: 26,
+        backgroundColor: "#2E333E",
+        height: "100%",
       }}
     >
-    
-        <Ionicons
-          name="ios-caret-back-circle-sharp"
-          size={40}
-          color={"white"}
-        />
-    
-    </TouchableOpacity>
-    <View>
-    <Text style={{fontSize:18 , padding:8 ,  color:"white"}}> Welcome</Text>
-    </View>
-    <View
-      style={{
-        width: 50,
-        height: 50,
-        overflow: "hidden",
-        borderRadius: 20,
-      }}
-    > 
-      
-         <TouchableOpacity
-       onPress={() => {
-          navigation.navigate("ProfileTab");
-        
-       }}
-     >
-        <Image
+      <View>
+        <View
           style={{
-            height: "100%",
-            width: "100%",
-            borderRadius: 20,
-           
+            flexDirection: "row",
+            justifyContent: "space-between",
           }}
-          source={require("../assets/coffee5.jpg")}
-        />
-        </TouchableOpacity>
-      
-    </View>
-  </View>
-  <View style={{ width: "80%", marginVertical: 30}}>
+        >
+          <TouchableOpacity
+            style={{
+              borderRadius: 25,
+              overflow: "hidden",
+              width: 50,
+              height: 40,
+            }}
+            onPress={ss}
+          >
+            <SimpleLineIcons name="logout" size={35} color="tomato" />
+          </TouchableOpacity>
+
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              overflow: "hidden",
+              borderRadius: 15,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("ProfileTab");
+              }}
+            >
+              <Image
+                style={{
+                  height: "100%",
+                  width: "100%",
+                  borderRadius: 15,
+                }}
+                source={{ uri: image }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ width: "80%", marginVertical: 30 }}>
           <Text
             style={{
               color: "white",
@@ -129,33 +130,97 @@ export default function ProfileScreen({navigation}) {
             Find the best coffee for you
           </Text>
         </View>
-         
-     
-           
-         
-       
-  </ScrollView>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextInput
+            placeholder="Search"
+            style={{
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+              width: "100%",
+              backgroundColor: "gray",
+              borderRadius: 10,
+              color: "white",
+              fontSize: 16,
+            }}
+            onChangeText={(text) => {
+              setSearchTerm(text);
+            }}
+          />
+        </View>
+        <View
+          style={{
+            padding: 16,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: "white",
+                fontWeight: "500",
+                letterSpacing: 1,
+              }}
+            >
+              Products
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={{
+              fontSize: 14,
+              color: "tomato",
+              fontWeight: "400",
+              marginRight: 5,
+            }}
+            // onPress={() => {
+            //   navigation.navigate("Products");
+            // }}
+          >
+            <Text
+              style={{
+                fontSize: 15,
+                color: "tomato",
+                fontWeight: "400",
+              }}
+            >
+              SeeAll
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-
-
-
-  //   <View style={{ flexDirection: "column", alignItems: "center" }}>
-  //   <TouchableOpacity
-  //     onPress={() => {
-  //       if (role === "admin") {
-  //         navigation.navigate("Admin");
-  //       } else {
-  //         navigation.navigate("Home");
-  //       }
-  //     }}
-  //   >
-  //     <Image
-  //       style={{ height: 50, width: 50, borderRadius: 75 }}
-  //       source={{uri: image}}
-  //     />
-  //   </TouchableOpacity>
-  //   <Text>{name}</Text>
-  // </View>
+        <View style={{ paddingLeft: 5 }}>
+          <FlatList
+            data={products.slice(0, 4)}
+            numColumns={2}
+            showsHorizontalScrollIndicator={true}
+            renderItem={(itemData) => {
+              return (
+                <ProductCard
+                  productName={itemData.item.productName}
+                  price={itemData.item.price}
+                  details={itemData.item.details}
+                  image={itemData.item.image}
+                  Rate={itemData.item.Rate}
+                  id={itemData.item.id}
+                />
+              );
+            }}
+          />
+        </View>
+      </View>
+    </View>
   );
 }
 
@@ -168,27 +233,27 @@ const styles = StyleSheet.create({
   },
   username: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
   },
   bio: {
     marginVertical: 10,
     paddingHorizontal: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
-  logout:{
+  logout: {
     fontSize: 18,
-    color:"gray",
+    color: "gray",
     padding: 12,
     borderRadius: 10,
-    backgroundColor:"white",
+    backgroundColor: "white",
   },
-  Cart:{
+  Cart: {
     fontSize: 18,
-    color:"gray",
+    color: "gray",
     padding: 12,
     borderRadius: 10,
     borderWidth: 1,
-    backgroundColor:"white",
-  }
+    backgroundColor: "white",
+  },
 });
