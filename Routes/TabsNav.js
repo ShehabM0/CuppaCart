d9599a881b926bbb41c817678e963858ed6f0ab7
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-04-13 06:27:58 +0200

    changed the arrangments of the  tab navigation

--- Routes/TabsNav.js
+++ Routes/TabsNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
-import { ProductStack, CartStack } from './ProductCartStack';
+import { ProductStack, CartStack } from "./ProductCartStack";
 import ProfileScreen from "../Screens/ProfileScreen";
 import { FontAwesome } from "@expo/vector-icons";
 import SettingsScreen from "../Screens/SettingsScreen";
@@ -12,13 +12,22 @@ const Tab = createBottomTabNavigator();
 const TabsNav = () => {
   return (
     <Tab.Navigator
-    screenOptions={{headerShown:false , tabBarShowLabel:false ,tabBarStyle:{backgroundColor:"#865439", height: 60,
-    position: 'absolute',
-    bottom:20,
-    right: 16,
-    left: 16,
-    borderRadius: 16,
-    shadowRadius:10 },tabBarInactiveTintColor:"white" ,tabBarActiveTintColor:"yellow"}}
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#865439",
+          height: 60,
+          position: "absolute",
+          bottom: 20,
+          right: 16,
+          left: 16,
+          borderRadius: 16,
+          shadowRadius: 10,
+        },
+        tabBarInactiveTintColor: "white",
+        tabBarActiveTintColor: "yellow",
+      }}
     >
       <Tab.Screen
         name="HomeTab"
@@ -30,11 +39,11 @@ const TabsNav = () => {
         }}
       />
       <Tab.Screen
-        name="CartTab"
-        component={CartStack}
+        name="Fav"
+        component={Fav}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="opencart" size={size} color={color} />
+            <Ionicons name="heart" size={size} color={color} />
           ),
         }}
       />
@@ -48,20 +57,21 @@ const TabsNav = () => {
         }}
       />
       <Tab.Screen
+        name="CartTab"
+        component={CartStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="opencart" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
         name="SettingsTab"
         component={SettingsScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="settings-outline" size={size} color={color} />
-          ),
-        }}
-      />
-       <Tab.Screen
-        name="Fav"
-        component={Fav}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="heart" size={size} color={color} />
           ),
         }}
       />
