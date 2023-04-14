6a6ab2aab156063775442455ef344fb9980e9dc1
Muhammed Walied <mw2242001@gmail.com>
2023-04-14 03:49:43 +0200

    Add Date

--- Screens/Sign-up.js
+++ Screens/Sign-up.js
@@ -3,12 +3,18 @@ import React,{useState} from 'react';
 import {
   View,
   Text,
-  SafeAreaView,
   Keyboard,
   ScrollView,
   Alert,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Platform
 } from 'react-native'; 
-
+import DatePicker from "react-native-modern-datepicker";
+import { getFormatedDate } from "react-native-modern-datepicker";
+import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from '../Conts/Color';
 import Button from '../Components/Button';
 import Input from '../Components/Input';
@@ -18,15 +24,22 @@ import auth from "../firebase/config";
 import { sendEmailVerification } from "firebase/auth";
 import { register, getUserUId } from "../firebase/auth";
 import { addUser } from "../firebase/user";
-
 const RegistrationScreen = ({navigation}) => {
   const [fullname, setFullname] = useState("");
+  const [isFocused, setIsFocused] = React.useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [errors, setErrors] = React.useState({});
   const [loading, setLoading] = React.useState(false);
-
+  const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
+  const today = new Date();
+  const startDate = getFormatedDate(
+    today.setDate(today.getDate() + 1),
+    "YYYY/MM/DD"
+  );
+  const [selectedStartDate, setSelectedStartDate] = useState("");
+  const [startedDate, setStartedDate] = useState("12/12/2023");
   const validate = async () => {
     Keyboard.dismiss();
     let isValid = true;
@@ -73,7 +86,8 @@ const RegistrationScreen = ({navigation}) => {
               "https://64.media.tumblr.com/d82d24956974272dff1f745a004a43bf/tumblr_o51oavbMDx1ugpbmuo3_540.png",
             cart: [],
             favorite:[],
-            balance:0
+            balance:0,
+            selectedStartDate,
           });
         });
         setLoading(true);
@@ -96,8 +110,15 @@ const RegistrationScreen = ({navigation}) => {
   const handleError = (error, input) => {
     setErrors(prevState => ({...prevState, [input]: error}));
   };
+  function handleChangeStartDate(propDate) {
+    setStartedDate(propDate);
+  }
+
+  const handleOnPressStartDate = () => {
+    setOpenStartDatePicker(!openStartDatePicker);
+  };
   return (
-    <SafeAreaView style={{backgroundColor: COLORS.white, flex: 1}}>
+    <View style={{backgroundColor: COLORS.white, flex: 1}}>
       <Loader visible={loading} />
       <ScrollView
         contentContainerStyle={{paddingTop: 50, paddingHorizontal: 20}}>
@@ -148,6 +169,67 @@ const RegistrationScreen = ({navigation}) => {
             error={errors.password}
             password
           />
+      <SafeAreaView style={{ flex: 1  ,padding:-65}}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : ""}
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#fff",
+          }}
+        >
+          <View >
+            <View style={{marginBottom: 5 ,width:"100%" ,}}>
+              <View>
+                <Text style={{marginVertical: 5,
+    fontSize: 14,
+    color: COLORS.grey, }}>Select Date</Text>
+                <TouchableOpacity
+                value={selectedStartDate}
+                onChangeText={setSelectedStartDate}
+                  style={styles.inputBtn}
+                  onPress={handleOnPressStartDate}
+                >
+                  <Text>{selectedStartDate}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+  
+            {/* Create modal for date picker */}
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={openStartDatePicker}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <DatePicker
+                    mode="calendar"
+                    minimumDate={startDate}
+                    selected={startedDate}
+                    onDateChanged={handleChangeStartDate}
+                    onSelectedChange={(date) => setSelectedStartDate(date)}
+                    options={{
+                      backgroundColor: "#080516",
+                      textHeaderColor: "#469ab6",
+                      textDefaultColor: "#FFFFFF",
+                      selectedTextColor: "#FFF",
+                      mainColor: "#469ab6",
+                      textSecondaryColor: "#FFFFFF",
+                      borderColor: "rgba(122, 146, 165, 0.1)",
+                    }}
+                  />
+                  <TouchableOpacity onPress={handleOnPressStartDate}>
+                    <Text style={{ color: "white" }}>Close</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+          
+  
           <Button title="Register" onPress={validate} />
           <Text
             onPress={() => navigation.navigate('SignIn')}
@@ -160,9 +242,62 @@ const RegistrationScreen = ({navigation}) => {
             Already have account ?Login
           </Text>
         </View>
+      
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
 export default RegistrationScreen;
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: 36,
+    marginVertical: 60,
+    color: "#111",
+  },
+  textSubHeader: {
+    fontSize: 25,
+    color: "#111",
+  },
+  inputBtn: {
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: "#222",
+    height: 50,
+    paddingLeft: 8,
+    fontSize: 18,
+    justifyContent: "center",
+    marginTop: 14,
+  },
+  submitBtn: {
+    backgroundColor: "#342342",
+    paddingVertical: 22,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    paddingVertical: 12,
+    marginVertical: 16,
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#080516",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    padding: 35,
+    width: "90%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
