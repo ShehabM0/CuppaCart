52f70eefebf6c225c96caf5e49547b76906ce65c
ShehabM0 <shehabmohamed2357@gmail.com>
2023-04-17 00:40:18 +0200

    added CreateCreditCard functionality

--- Screens/CreateCreditCard.js
+++ Screens/CreateCreditCard.js
@@ -1,12 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'; 
 
+import SuccessMessage from '../Components/SuccessMessage';
+import Loader from '../Components/Loader';
 import Input from '../Components/Input';
+
+import { collection, getDocs, addDoc, query, where } from 'firebase/firestore';
+import { db } from '../firebase/config';
 
 export default RegistrationScreen = ({ navigation }) => {
 
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [date, setDate] = useState('');
+    const [cvv, setCvv] = useState('');
+    const [balance, setBalance] = useState('');
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const creditCardCollection = collection(db, "creditcards");
+
+    const handleSubmit = async () => {
+        if(validateName() && validateNumber() && validateDate() && validateCvv() && validateBalance()) {
+            const q = query(
+                creditCardCollection, 
+                where("name", "==", name.trim().toLowerCase()),
+                where("number" , "==", number),
+                where("expiry_date" , "==", date),
+                where("cvv" , "==", cvv),);
+            const existCreditCard = await getDocs(q);
+
+            if(existCreditCard.docs.length) {
+                alert("Invalid Credit Card Info!");
+            } else {
+                creditCardData = {
+                    name: name.trim().toLowerCase(),
+                    number: number,
+                    expiry_date: date,
+                    cvv: cvv,
+                    balance: Number(balance)
+                }
+                await addDoc(creditCardCollection, creditCardData)
+                .then(() => {
+                    setLoading(true);
+                    setTimeout(() => setLoading(false), 2000);
+
+                    setTimeout(() => {
+                        setSuccess(true);
+                        setTimeout(() => setSuccess(false), 3500);
+                    }, 2000);
+
+                    setTimeout(() => {
+                        navigation.navigate('TabsNav');
+                    }, 5500);
+                })
+                .catch(error => alert(error.message));
+            }
+        } else {
+            alert("Invalid Credit Card Info!");
+        }
+    }
+
+
+    function validateName() {
+        if(!name) {
+            handleError('Please enter name', 'name');
+            return false;
+        }
+        return true;
+    }
+    function validateNumber() {
+        if(!number.match(/^\d+$/) || number.length != 16) {
+            handleError('Credit Card number must be 16 digits', 'number');
+            return false;
+        }
+        return true;
+    }
+    function validateDate() {
+        if(!date.includes('/') || date.length != 5) {
+            handleError('Credit Card expiration date must be in form MM/YY', 'date');
+            return false;
+        }
+        return true;
+    }
+    function validateCvv() {
+        if(!cvv.match(/^\d+$/) || cvv.length in [3, 4]) {
+            handleError('Credit Card CVV must consist of only 3-4 digits', 'cvv');
+            return false;
+        }
+        return true;
+    }
+    function validateBalance() {
+        return (balance > -1 &&  !isNaN(balance));
+    }
+
+    const handleError = (error, input) => {
+        setErrors(prevState => ({...prevState, [input]: error}));
+    };
+
     return (
         <SafeAreaView style={{backgroundColor: COLORS.white, flex: 1}}>
+            <Loader visible={loading} />
             <ScrollView contentContainerStyle={{paddingTop: 50, paddingHorizontal: 20}}>
                 <Text style={{color: COLORS.black, fontSize: 40, fontWeight: 'bold'}}>
                     CreditCard
@@ -19,12 +114,20 @@ export default RegistrationScreen = ({ navigation }) => {
                         iconName="credit-card-outline"
                         label="Name on Card"
                         placeholder="Name on the front of your card"
+                        value={name}
+                        onChangeText={setName}
+                        onFocus={() => handleError(null, name)}
+                        error={errors.name}
                     />
 
                     <Input
                         iconName="numeric"
                         label="Card Number"
                         placeholder="Digits on the front of your card"
+                        value={number}
+                        onChangeText={setNumber}
+                        onFocus={() => handleError(null, number)}
+                        error={errors.number}
                     />
 
                     <View style={styles.cont}>
@@ -33,6 +136,10 @@ export default RegistrationScreen = ({ navigation }) => {
                                 iconName="timetable"
                                 label="Expiration Date"
                                 placeholder="MM/YY"
+                                value={date}
+                                onChangeText={setDate}
+                                onFocus={() => handleError(null, date)}
+                                error={errors.date}
                             />
                         </View>
                         <View style={{width: "50%"}}>
@@ -41,6 +148,10 @@ export default RegistrationScreen = ({ navigation }) => {
                                 iconName="credit-card-lock"
                                 label="CVV"
                                 placeholder="3-4 digits"
+                                value={cvv}
+                                onChangeText={setCvv}
+                                onFocus={() => handleError(null, cvv)}
+                                error={errors.cvv}
                             />
                         </View>
                     </View>
@@ -50,16 +161,24 @@ export default RegistrationScreen = ({ navigation }) => {
                         iconName="cash"
                         label="Balance"
                         placeholder="Credit Card Balance"
+                        value={balance}
+                        onChangeText={setBalance}
+                        onFocus={() => handleError(null, balance)}
+                        error={errors.balance}
                     />
                     
                     <View style={{ alignItems: 'center' }}>
-                        <TouchableOpacity activeOpacity={0.7} style={styles.submitBtn}>
+                        <TouchableOpacity activeOpacity={0.7} style={styles.submitBtn} onPress={handleSubmit}>
                             <Text style={styles.btnTxt}> Create </Text>
                         </TouchableOpacity>
                     </View>
 
                 </View>
             </ScrollView>
+            {
+                success &&
+                <SuccessMessage message={"Credit Card has been add to database"}/>
+            }
         </SafeAreaView>
     );
 }
