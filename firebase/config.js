4f4bd5b6837f493616d812ac5952719b6950f4bb
mokhtar <mohamedmokhtarm3@gmail.com>
2023-04-18 04:44:38 +0200

    Add firebase storage package to config.js and handled it.

--- firebase/config.js
+++ firebase/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAIg3oEpdeXbM2TeDArxg41lbrUdpaDetA",
@@ -17,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-export { app, auth , db };
+const storage = getStorage(app);
+export { app, auth , db , storage };
