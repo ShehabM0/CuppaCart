8818cb53e7c6269fc5d0c1677f08a68225e3578c
ShehabM0 <shehabmohamed2357@gmail.com>
2023-04-23 21:14:19 +0200

    added ReviewCard Component

---
+++ Components/ReviewCard.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
+
+import ReviewCardStars from "../Components/ReviewCardStars"
+
+export default function ReviewCard({ userName, reviewStars, reviewText }) {
+
+  const [readMore, setReadMore] = useState(false);
+
+  handleTextLayout = (e) => {
+    const numberOfLines = e.nativeEvent.lines.length;
+    if(numberOfLines >= 3)
+      setReadMore(true);
+  }
+
+  return (
+    <View style={styles.cardCont}>
+        <View style={styles.user_ImgNameRev_Cont}>
+            <View style={{ flexDirection: 'row' }}>
+                <Image
+                    source={require('../assets/user_icon.png')}
+                    style={styles.userImg}
+                />
+                <Text ellipsizeMode='tail' numberOfLines={1} style={styles.userName}> { userName } </Text>
+            </View>
+            <View style={{ paddingLeft: 5 }}>
+                <ReviewCardStars number={ reviewStars }/>
+            </View>
+        </View>
+        <View style={{ paddingVertical: 10 }}>
+            <Text onTextLayout={handleTextLayout} style={{ color: 'rgba(0, 0, 0, 0.7)' }}>
+              { reviewText }
+            </Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardCont: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      marginVertical: 10,
+      borderRadius: 5,
+      width: '90%',
+      padding: 20,
+  },
+  user_ImgNameRev_Cont: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+      marginBottom: 5,
+  },
+  userImg: {
+      width: 25,
+      height: 25,
+  },
+  userName: {
+    width: 120,
+    fontWeight: 'bold'
+  },
+});
