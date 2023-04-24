e10b363acc00b75abcf8178d397a53f208fc98e0
mokhtar <mohamedmokhtarm3@gmail.com>
2023-04-24 14:26:16 +0200

    create delete my account function and handel it

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,13 +1,113 @@
 import React,{useEffect,useState} from "react";
 import { SafeAreaView,ScrollView,View,Text,StyleSheet,currentUser,Alert } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import {auth} from "../firebase/config";
 import { TouchableRipple} from "react-native-paper";
+import {auth,db} from "../firebase/config";
 import {getUserById,deleteUser} from "../firebase/user";
+import { doc, deleteDoc } from "firebase/firestore";
+import { getUserUId } from "../firebase/auth";
 import { getFormatedDate } from "react-native-modern-datepicker";
 import * as ImagePicker from "expo-image-picker";
 
 const SettingsScreen = ({navigation}) => {
+
+
+
+
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [image, setimage] = useState();
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = React.useState({});
+  const [role, setRole] = useState("");
+
+  const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
+  const today = new Date();
+  const startDate = getFormatedDate(
+      today.setDate(today.getDate() - 36500),
+      "YYYY/MM/DD"
+  );
+  const [selectedStartDate, setSelectedStartDate] = useState("");
+  const [startedDate, setStartedDate] = useState("12/12/2023");
+  const[isLoading,setIsLoading]=useState(false);
+
+
+  useEffect(() => {
+      getUserUId().then((id) => {
+          //console.log(id);
+          getUserById(id).then((user) => {
+              setimage(user[0].image);
+              setPhone(user[0].phone);
+              setEmail(user[0].email);
+              setFirstname(user[0].firstname);
+              setLastname(user[0].lastname);
+              setSelectedStartDate(user[0].selectedStartDate)
+              setRole(user[0].Role)
+          });
+      });
+  }, []);
+
+
+  const handleDelete= () => {
+    Alert.alert(
+      "Delete Account?",
+      "Are you sure you want to delete your account?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: async () => {
+            try {
+              const user = auth.currentUser;
+              getUserUId().then((id)=>{
+             const uid = id
+              const docRef = doc(db, "users", uid);
+               deleteDoc(docRef);
+               user.delete();
+              navigation.navigate("SignIn");
+            }
+    )} catch (error) {
+              console.log(error);
+            }
+          },
+          style: "destructive",
+        },
+      ]
+    );
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return(
     <SafeAreaView style={{flex:1,backgroundColor:'#f6f6f6'}}>
     <ScrollView contentContainerStyle={styles.container}>
@@ -30,7 +130,7 @@ const SettingsScreen = ({navigation}) => {
             </View>
           </TouchableRipple>  
 
-          <TouchableRipple onPress={() => {}}>
+          <TouchableRipple onPress={handleDelete}>
             <View style={styles.menuItem}>
               <Icon name="account-remove" color='red' size={25} />
               <Text style={styles.menuItemText}>Delete my account</Text>
