8d29a9a966a9efe914ae6c28e5620500aa1241fd
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-04-25 04:27:15 +0200

    added change password navigation

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,55 +1,55 @@
-import React,{useEffect,useState} from "react";
-import { SafeAreaView,ScrollView,View,Text,StyleSheet,currentUser,Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  currentUser,
+  Alert,
+} from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { TouchableRipple} from "react-native-paper";
-import {auth,db} from "../firebase/config";
-import {getUserById,deleteUser} from "../firebase/user";
+import { TouchableRipple } from "react-native-paper";
+import { auth, db } from "../firebase/config";
+import { getUserById, deleteUser } from "../firebase/user";
 import { doc, deleteDoc } from "firebase/firestore";
 import { getUserUId } from "../firebase/auth";
 import { getFormatedDate } from "react-native-modern-datepicker";
-import * as ImagePicker from "expo-image-picker";
 
-const SettingsScreen = ({navigation}) => {
-
-
-
-
+const SettingsScreen = ({ navigation }) => {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [image, setimage] = useState();
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [errors, setErrors] = React.useState({});
   const [role, setRole] = useState("");
 
   const [openStartDatePicker, setOpenStartDatePicker] = useState(false);
   const today = new Date();
   const startDate = getFormatedDate(
-      today.setDate(today.getDate() - 36500),
-      "YYYY/MM/DD"
+    today.setDate(today.getDate() - 36500),
+    "YYYY/MM/DD"
   );
   const [selectedStartDate, setSelectedStartDate] = useState("");
   const [startedDate, setStartedDate] = useState("12/12/2023");
-  const[isLoading,setIsLoading]=useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-      getUserUId().then((id) => {
-          //console.log(id);
-          getUserById(id).then((user) => {
-              setimage(user[0].image);
-              setPhone(user[0].phone);
-              setEmail(user[0].email);
-              setFirstname(user[0].firstname);
-              setLastname(user[0].lastname);
-              setSelectedStartDate(user[0].selectedStartDate)
-              setRole(user[0].Role)
-          });
+    getUserUId().then((id) => {
+      //console.log(id);
+      getUserById(id).then((user) => {
+        setimage(user[0].image);
+        setPhone(user[0].phone);
+        setEmail(user[0].email);
+        setFirstname(user[0].firstname);
+        setLastname(user[0].lastname);
+        setSelectedStartDate(user[0].selectedStartDate);
+        setRole(user[0].Role);
       });
+    });
   }, []);
 
-
-  const handleDelete= () => {
+  const handleDelete = () => {
     Alert.alert(
       "Delete Account?",
       "Are you sure you want to delete your account?",
@@ -63,14 +63,14 @@ const SettingsScreen = ({navigation}) => {
           onPress: async () => {
             try {
               const user = auth.currentUser;
-              getUserUId().then((id)=>{
-             const uid = id
-              const docRef = doc(db, "users", uid);
-               deleteDoc(docRef);
-               user.delete();
-              navigation.navigate("SignIn");
-            }
-    )} catch (error) {
+              getUserUId().then((id) => {
+                const uid = id;
+                const docRef = doc(db, "users", uid);
+                deleteDoc(docRef);
+                user.delete();
+                navigation.navigate("SignIn");
+              });
+            } catch (error) {
               console.log(error);
             }
           },
@@ -80,95 +80,68 @@ const SettingsScreen = ({navigation}) => {
     );
   };
 
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f6f6f6" }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Settings</Text>
+          <Text style={styles.subtitle}>Update your preferences here</Text>
+        </View>
 
+        <TouchableRipple
+          onPress={() => {
+            navigation.navigate("EditUserScreen");
+          }}
+        >
+          <View style={styles.menuItem}>
+            <Icon name="account-edit" color="orange" size={25} />
+            <Text style={styles.menuItemText}>Edit my profile</Text>
+          </View>
+        </TouchableRipple>
 
+        <TouchableRipple
+          onPress={() => {
+            navigation.navigate("ChangePassword");
+          }}
+        >
+          <View style={styles.menuItem}>
+            <Icon name="key-change" color="blue" size={25} />
+            <Text style={styles.menuItemText}>Change my password</Text>
+          </View>
+        </TouchableRipple>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return(
-    <SafeAreaView style={{flex:1,backgroundColor:'#f6f6f6'}}>
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Settings</Text>
-        <Text style={styles.subtitle}>Update your preferences here</Text>
-      </View>
-
-      <TouchableRipple onPress={() => {navigation.navigate("EditUserScreen");}}>
-            <View style={styles.menuItem}>
-              <Icon name="account-edit" color='orange' size={25} />
-              <Text style={styles.menuItemText}>Edit my profile</Text>
-            </View>
-          </TouchableRipple>
-
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="key-change" color='blue' size={25} />
-              <Text style={styles.menuItemText}>Change my password</Text>
-            </View>
-          </TouchableRipple>  
-
-          <TouchableRipple onPress={handleDelete}>
-            <View style={styles.menuItem}>
-              <Icon name="account-remove" color='red' size={25} />
-              <Text style={styles.menuItemText}>Delete my account</Text>
-            </View>
-          </TouchableRipple>    
-
-
-
-
-
-
-    </ScrollView>
+        <TouchableRipple onPress={handleDelete}>
+          <View style={styles.menuItem}>
+            <Icon name="account-remove" color="red" size={25} />
+            <Text style={styles.menuItemText}>Delete my account</Text>
+          </View>
+        </TouchableRipple>
+      </ScrollView>
     </SafeAreaView>
-);
-
-}
+  );
+};
 
 export default SettingsScreen;
 
-const styles=StyleSheet.create({
-  container:{
-    paddingVertical:24
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
   },
-  header:{
-    paddingHorizontal:24,
-    marginBottom:12
+  header: {
+    paddingHorizontal: 24,
+    marginBottom: 12,
   },
-  title:{
-    fontSize:32,
-    fontWeight:'700',
-    color:'#1d1d1d',
-    marginBottom:6,
-    marginTop:20
+  title: {
+    fontSize: 32,
+    fontWeight: "700",
+    color: "#1d1d1d",
+    marginBottom: 6,
+    marginTop: 20,
   },
-  subtitle:{
-    fontSize:15,
-    fontWeight:'500',
-    color:'#929292'
+  subtitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#929292",
   },
   menuWrapper: {
     marginTop: 10,
@@ -186,4 +159,4 @@ const styles=StyleSheet.create({
     fontSize: 16,
     lineHeight: 26,
   },
-})
+});
