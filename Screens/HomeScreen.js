6003781c38f40e923376d94b5038eb2b76d6d448
Muhammed Walied <mw2242001@gmail.com>
2023-04-27 22:38:25 +0200

    edit home page

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
-  
+  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
@@ -8,25 +8,22 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
-  ImageBackground,
-  FlatList,
-  TextInput,
-  
 } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar';
+import { SearchBar } from 'react-native-elements';
 
 import Carousel from 'react-native-snap-carousel';
 import Card from '../Components/coffeeCard';
 import { auth } from "../firebase/config";
-import { SimpleLineIcons } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
 import { getUserUId, getUserById } from "../firebase/user";
 import { getProducts } from "../firebase/products";
 import ProductCard from "../Components/productCard";
 import { logout } from "../firebase/auth";
 import { BellIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 import { MapPinIcon } from 'react-native-heroicons/solid'
+import { BlurView } from "expo-blur";
+import { Ionicons } from "@expo/vector-icons";
+
 const { width } = Dimensions.get("window");
 const d = Dimensions.get("window");
 export default function ProfileScreen({ navigation }) {
@@ -34,30 +31,6 @@ export default function ProfileScreen({ navigation }) {
   const [image, setimage] = useState(null);
   const [role, setRole] = useState("");
   const [products, setProducts] = useState([]);
-  const [activeCategory, setActiveCategory] = useState(1);
-   const categories = [
-    {
-      id: 1,
-      title: "Cappuccino",
-    },
-    {
-      id: 2,
-      title: "Latte",
-    },
-    {
-      id: 3,
-      title: "Espresso",
-    },
-    {
-      id: 4,
-      title: "Mocha",
-    },
-    {
-      id: 5,
-      title: "Americano",
-    },
-  ]
-  
   const getProductHandle = async () => {
     const arr = await getProducts();
     setProducts(arr);
@@ -82,78 +55,99 @@ export default function ProfileScreen({ navigation }) {
     });
   }, []);
   return (
-    <View className="flex-1 relative bg-white">
-  <StatusBar />
-
-    <Image 
-      source={require('../assets/beansBackground1.png')} 
-      style={{height: 220}} 
-      className="w-full absolute -top-5 opacity-10" />
-    <SafeAreaView className="flex-1">
-      {/* avatar and bell icon */}
-      <View className="mx-4 flex-row justify-between items-center">
-        <TouchableOpacity  onPress={() => {
-              navigation.navigate("ProfileTab");
-            }}>
-        <Image source={require('../assets/avatar.png')} 
-          className="h-9 w-9 rounded-full" />
-        </TouchableOpacity>
-        <View className="flex-row items-center space-x-2">
-          <MapPinIcon size="25" color={ '#d4a574'} />
-          <Text className="font-semibold text-base">
-            New York, NYC
-          </Text>
-        </View>
-        <BellIcon size="27" color="black" />
-      </View>
-      {/* search bar */}
-      <View className="mx-5 mt-14 shadow">
-        <View className="flex-row items-center rounded-full p-1 bg-[#e6e6e6]">
-          <TextInput placeholder='Search' className="p-4 flex-1 font-semibold text-gray-700" />
-          <TouchableOpacity 
-            className="rounded-full p-2" 
-            style={{backgroundColor:  '#d4a574'}}>
-            <MagnifyingGlassIcon size="25" strokeWidth={2} color="white" />
-          </TouchableOpacity>
-        </View>
-      </View>
-      {/* categories */}
-      <View className="px-5 mt-6">
-
-        <FlatList 
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={categories}
-          keyExtractor={item=> item.id}
-          className="overflow-visible"
-          renderItem={({item})=>{
-            isActive = item.id==activeCategory;
-            let activeTextClass = isActive? 'text-white': 'text-gray-700';
-            return (
-              <TouchableOpacity 
-              onPress={()=> setActiveCategory(item.id)}
-              style={{backgroundColor: isActive?  '#d4a574': 'rgba(0,0,0,0.07)'}} 
-              className="p-4 px-5 mr-2 rounded-full shadow">
-                <Text className={"font-semibold " + activeTextClass}>{item.title}</Text>
-              </TouchableOpacity>
-            )
+    <SafeAreaView style={{backgroundColor:"#361500", height:"100%"}}>
+    <ScrollView
+      style={{
+        padding: 10,
+     
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            borderRadius: 10,
+            overflow: "hidden",
+            width: 10 * 4,
+            height: 10 * 4,
           }}
-        />
+        >
+          <BlurView
+            style={{
+              height: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Ionicons
+              name="menu"
+              size={10 * 2.5}
+              color={"#52555A"}
+            />
+          </BlurView>
+        </TouchableOpacity>
+        <View
+          style={{
+            width: 10 * 4,
+            height: 10 * 4,
+            overflow: "hidden",
+            borderRadius: 10,
+          }}
+        >
+          <BlurView
+            style={{
+              height: "100%",
+              padding: 10 / 2,
+            }}
+          >
+            <Image
+              style={{
+                height: "100%",
+                width: "100%",
+                borderRadius: 10,
+              }}
+              source={{uri:image}}
+            />
+          </BlurView>
+        </View>
       </View>
-        
-        {/* coffee cards */}
-       <View className="mt-16 py-2">
-    <Carousel
-      data={products}
-      firstItem={1}
-      inactiveSlideScale={0.77}
-      inactiveSlideOpacity={0.75}
-      sliderWidth={400}
-      itemWidth={260}
+      <View style={{ width: "80%", marginVertical: 10 * 3 }}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 10 * 3.5,
+            fontWeight: "600",
+          }}
+        >
+          Find the best coffee for you
+        </Text>
+      </View>
+      <SearchBar
+        placeholder="Type Here..."
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        <Carousel
+     data={products}
+     firstItem={1}
+     inactiveSlideScale={0.77}
+     inactiveSlideOpacity={0.75}
+     sliderWidth={400}
+     itemWidth={180}
+  
       slideStyle={{display: 'flex', alignItems: 'center'}}
       renderItem={(itemData) => {
         return (
-          <Card
+          <ProductCard
             productName={itemData.item.productName}
             price={itemData.item.price}
             details={itemData.item.details}
@@ -164,45 +158,10 @@ export default function ProfileScreen({ navigation }) {
         );
       }}
     />
-     </View>
-    </SafeAreaView>
-    
-    
-  </View>
+      </View>
+    </ScrollView>
+  </SafeAreaView>
  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  username: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 10,
-  },
-  bio: {
-    marginVertical: 10,
-    paddingHorizontal: 30,
-    textAlign: "center",
-  },
-  logout: {
-    fontSize: 18,
-    color: "gray",
-    padding: 12,
-    borderRadius: 10,
-    backgroundColor: "white",
-  },
-  Cart: {
-    fontSize: 18,
-    color: "gray",
-    padding: 12,
-    borderRadius: 10,
-    borderWidth: 1,
-    backgroundColor: "white",
-  },
-});
