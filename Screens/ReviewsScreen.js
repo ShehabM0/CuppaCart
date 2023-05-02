edfee9e5cef9e3d18cb8ace30ad5f4251a551264
ShehabM0 <shehabmohamed2357@gmail.com>
2023-05-02 12:22:50 +0300

    fetched the data from the database

--- Screens/ReviewsScreen.js
+++ Screens/ReviewsScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, StatusBar, ScrollView, SafeAreaView } from "react-native";
 
 import ReviewButtonLink from "../Components/ReviewButtonLink";
 import ReviewCard from "../Components/ReviewCard";
+import { getReviews } from "../firebase/reviews"
 
-export default function ReviewsScreen() {
+export default function ReviewsScreen({ route }) {
+
+  const { product_id } = route.params;
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews(product_id)
+    .then(data => setReviews(data))
+    .catch(err => alert(err.message))
+  }, []);
+
   return (
     <SafeAreaView style={{ paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 2 : 0,}}>
       <ScrollView>
@@ -12,26 +23,19 @@ export default function ReviewsScreen() {
           image={'addImg'}
           name={"Add Review"}
           nav={"AddReview"}
+          product_id={product_id}
         />
 
         <View style={styles.reviewsCont}>
-          <ReviewCard
-            userName={'Shehab Mohamed'}
-            reviewStars={4}
-            reviewText={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "}
-          />
-
-          <ReviewCard
-            userName={'Shehab Mohamed'}
-            reviewStars={2}
-            reviewText={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "}
-          />
-
-          <ReviewCard 
-            userName={'Shehab Mohamed'}
-            reviewStars={5}
-            reviewText={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "}
-          />
+          {reviews.map((reviewData) => {
+            return (
+              <ReviewCard
+                userName={reviewData.user_name}
+                reviewStars={reviewData.stars}
+                reviewText={reviewData.text}
+              />
+            );
+          })}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -42,5 +46,6 @@ const styles = StyleSheet.create({
   reviewsCont: {
     justifyContent: 'center',
     alignItems: 'center',
+    paddingBottom: 200
   }
 });
