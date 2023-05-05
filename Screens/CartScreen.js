fdebaa469efb3e663be20e80aba8115868161e42
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-05-06 00:27:18 +0300

    new Checkout and Cart Screens

--- Screens/CartScreen.js
+++ Screens/CartScreen.js
@@ -1,10 +1,17 @@
-import { View, Text,FlatList } from 'react-native'
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 
 import { getProducts } from "../firebase/products";
-import Cartcard from '../Components/cartcard';
+import Cartcard from "../Components/cartcard";
+import * as Haptics from "expo-haptics";
 
-const CartScreen = () => {
+const CartScreen = ({ navigation }) => {
   const [products, setProducts] = useState([]);
 
   const getProductHandle = async () => {
@@ -15,29 +22,56 @@ const CartScreen = () => {
   useEffect(() => {
     getProductHandle();
   }, []);
-  return (
-    <View>
-      <View style={{ marginVertical: 20 }}>
-          <View>
-            <FlatList
-              data={products}
-              renderItem={(itemData) => {
-                return (
-                  <Cartcard
-                  productName={itemData.item.productName}
-                  price={itemData.item.price}
-                  details={itemData.item.details}
-                  image={itemData.item.image}
-                  Rate={itemData.item.Rate}
-                  id={itemData.item.id}
-                  />
-                );
-              }}
-            />
-          </View>
-        </View>
-    </View>
-  )
-}
 
-export default CartScreen
+  function checkoutnavigation() {
+    navigation.navigate("Checkout");
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+  }
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={products}
+          renderItem={(itemData) => {
+            return (
+              <Cartcard
+                productName={itemData.item.productName}
+                price={itemData.item.price}
+                details={itemData.item.details}
+                image={itemData.item.image}
+                Rate={itemData.item.Rate}
+                id={itemData.item.id}
+              />
+            );
+          }}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={checkoutnavigation}>
+        <Text style={styles.buttonText}>Proceed to checkout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  button: {
+    width: 350,
+    backgroundColor: "#C67C4E",
+    padding: 20,
+    borderRadius: 20,
+    alignItems: "center",
+    marginTop: 1,
+    marginLeft: 20,
+    top: -10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
+
+export default CartScreen;
