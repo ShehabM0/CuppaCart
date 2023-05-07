23818cb30f297d22049d0cc3c80035d206ee27d6
Muhammed Walied <mw2242001@gmail.com>
2023-05-07 22:50:08 +0300

    Add Delete from Fav Function

--- Components/FavCard.js
+++ Components/FavCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
 import {
   View,
   Text,
@@ -10,6 +10,18 @@ import {
 import { useNavigation } from "@react-navigation/core";
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  getUserUId,
+  addUser,
+  deleteUser,
+  editUser,
+  getUserById,
+  getUserByName,
+  getUsers,
+  subscribeUser,
+  getCurrUserId,
+  updateUser,
+  } from "../firebase/user";
 
 const FavCard = ({
   productName,
@@ -26,7 +38,45 @@ const FavCard = ({
   const { width } = Dimensions.get("window");
 
  
+  const [userFav, setUserFav] = useState([]);
+  const [user, setUser] = useState();
+  const [ProductInFav, setProductInFav] = useState();
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [role, setRole] = useState("");
+  const [userCart, setUserCart] = useState([]);
+  const user_id = getCurrUserId();
 
+  useEffect(() => {
+    getUserUId().then((id) => {
+      getUserById(id).then((user) => {
+        user.forEach((user) => {
+          console.log("fav is ", user.favorite);
+          setUserFav(user.favorite);
+          setUser(user);
+        });
+        setLoading(false); // Hide the loader when the data fetching is complete
+      });
+    });
+  }, []);
+  
+  const handleDelete = async () => {
+    // var userfavorite = user.favorite;
+    // userfavorite = userfavorite.filter((e) => e !== id);
+    // console.log("here",userfavorite);
+    // editUser({
+    //   ...user,
+    //   favorite: userfavorite,
+    // });
+    // alert("Product Deleted From Cart");
+    const id_idx = userFav.indexOf(id);
+    userFav.splice(id_idx, 1);
+    updateUser(user_id, { favorite: [...userFav] });
+    alert("Product Deleted From Cart");
+  };
   return (
     
             
@@ -61,7 +111,7 @@ const FavCard = ({
               <View style={styles.priceContainer}>
                 <Text style={styles.price}>{price} $</Text>
 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={() => {handleDelete()}}>
                 <View style={styles.menuItem}>
                   <Ionicons name="trash-outline" size={25} color="#fff" />
                   </View>
