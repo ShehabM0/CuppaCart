6c15e36adb18b7c5c1458b8807de0e7fd4e94c08
hazemmuuhammed <hazemmuuhammed@gmail.com>
2023-05-14 22:48:14 +0300

    added the email & phone id

--- Screens/CheckoutScreen.js
+++ Screens/CheckoutScreen.js
@@ -12,53 +12,63 @@ import React, { useState, useEffect } from "react";
 import BasicProductList from "../Components/BasicProductList";
 import * as Font from "expo-font";
 import * as LocalAuthentication from "expo-local-authentication";
-import SuccessMessage from '../Components/SuccessMessage';
-import Loader from '../Components/Loader';
-
+import SuccessMessage from "../Components/SuccessMessage";
+import Loader from "../Components/Loader";
+import { getUserUId, getUserById } from "../firebase/user";
 
 import { getProductByID } from "../firebase/products";
 import { getCreditCardById } from "../firebase/creditcard";
-import { 
-  getTotalCash, 
-  getTotalSum, getTotalQnt, 
-  orderCart, minusUserCash, minusProductQnt, 
-  addUserBonus 
+import {
+  getTotalCash,
+  getTotalSum,
+  getTotalQnt,
+  orderCart,
+  minusUserCash,
+  minusProductQnt,
+  addUserBonus,
 } from "../firebase/cart";
-
 
 import { getProducts } from "../firebase/products";
 const CheckoutScreen = ({ navigation }) => {
-
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
   const [Products, setProducts] = useState([]);
   const [index, setIndex] = useState("Cash");
   const [fontLoaded, setFontLoaded] = useState(false);
-
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [totalInCash, setTotalInCash] = useState(0);
 
   useEffect(() => {
     let b = 0;
     const getTotal = async () => {
       let a = 0;
-      await getTotalCash()
-      .then(total => a = total);
+      await getTotalCash().then((total) => (a = total));
       b = a;
-    }
-    getTotal()
-    .then(() => setTotalInCash(b));    
-  }, []);
+    };
 
+    getTotal().then(() => setTotalInCash(b));
+  }, [totalInCash]);
+
+  useEffect(() => {
+    getUserUId().then((id) => {
+     
+      getUserById(id).then((user) => {
+        
+        setEmail(user[0].email);
+        setPhone(user[0].phone);
+      });
+    });
+  }, []);
 
   function CheckOut() {
     setLoading(true);
     setTimeout(() => setLoading(false), 3500);
-    orderCart()
-    .then(result => {
-      if(result) {
+    orderCart().then((result) => {
+      if (result) {
         setTimeout(() => {
-            setSuccess(true);
-            setTimeout(() => setSuccess(false), 4000);
+          setSuccess(true);
+          setTimeout(() => setSuccess(false), 4000);
         }, 3500);
       }
     });
@@ -132,27 +142,23 @@ const CheckoutScreen = ({ navigation }) => {
 
   return (
     <>
-    <Loader visible={loading} />
-    {
-      success && 
-      <SuccessMessage message={"You payment has been done"}/>
-    }
+      <Loader visible={loading} />
+      {success && <SuccessMessage message={"You payment has been done"} />}
 
-    <View style={styles.container}>
-      <View style={styles.topBarContainer}>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.goBack();
-          }}
-         
-        >
-          <Ionicons
-            name="arrow-back-circle-outline"
-            size={30}
-            color={"#707981"}
-          />
-        </TouchableOpacity>
-        <View style={{ flex: 2, alignItems: "center", right: 23 }}>
+      <View style={styles.container}>
+        <View style={styles.topBarContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Ionicons
+              name="arrow-back-circle-outline"
+              size={30}
+              color={"#707981"}
+            />
+          </TouchableOpacity>
+          <View style={{ flex: 2, alignItems: "center", right: 23 }}>
             <Text
               style={{
                 color: "#1C0A00",
@@ -160,186 +166,190 @@ const CheckoutScreen = ({ navigation }) => {
                 fontFamily: "Sora-SemiBold",
                 textAlign: "center",
               }}
-              >
-              Order
-            </Text>
-        </View>
-        <View></View>
-      </View>
-      <ScrollView style={styles.bodyContainer} nestedScrollEnabled={true}>
-        <Text style={styles.primaryText}>Order Summary</Text>
-        <ScrollView
-          style={[styles.orderSummaryContainer]}
-          nestedScrollEnabled={true}
-        >
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={Products}
-            horizontal={false}
-            renderItem={(itemData) => {
-              return (
-                <BasicProductList
-                  productName={itemData.item.productName}
-                  price={itemData.item.price}
-                  image={itemData.item.image}
-                  id={itemData.item.id}
-                />
-              );
-            }}
-          />
-        </ScrollView>
-        <View style={{ marginTop: 20, flexDirection: "row" }}>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              paddingVertical: 10,
-              paddingHorizontal: 14,
-              width: 153.5,
-              height: 40,
-              borderRadius: 10,
-              gap: 10,
-              flex: 0,
-              flexGrow: 1,
-              backgroundColor: index === "Cash" ? "#C67C4E" : "#F2F2F2",
-            }}
-          >
-            <TouchableOpacity>
-              <Text
-                style={{
-                  width: 59,
-                  height: 20,
-                  fontFamily: "sora-regular",
-                  fontStyle: "normal",
-                  fontWeight: "600",
-                  fontSize: 16,
-                  lineHeight: 20,
-                  color: index === "Cash" ? "white" : "black",
-                  flex: 0,
-                  flexGrow: 0,
-                }}
-                onPress={() => setIndex("Cash")}
-              >
-                Cash
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              paddingVertical: 10,
-              paddingHorizontal: 14,
-              gap: 10,
-              width: 153.5,
-              height: 40,
-              backgroundColor: index === "Coins" ? "#C67C4E" : "#F2F2F2",
-              borderRadius: 10,
-              flex: 0,
-              flexGrow: 1,
-            }}
-          >
-            <TouchableOpacity>
-              <Text
-                style={{
-                  width: 62,
-                  height: 20,
-                  fontFamily: "sora-regular",
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  fontSize: 16,
-                  lineHeight: 20,
-                  color: index === "Coins" ? "white" : "black",
-                  flex: 0,
-                  marginLeft: 25,
-                  flexGrow: 0,
-                }}
-                onPress={() => setIndex("Coins")}
-              >
-                Coins
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        {index === "Cash" ? (
-          <View>
-            <Text style={styles.primaryText}>Total</Text>
-            <View style={styles.totalOrderInfoContainer}>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Order</Text>
-                <Text style={styles.secondaryTextSm}>{totalInCash}$</Text>
-              </View>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Delivery</Text>
-                <Text style={styles.secondaryTextSm}>{totalInCash * 0.05} $</Text>
-              </View>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Total In Cash</Text>
-                <Text style={styles.secondaryTextSm}>{totalInCash + (totalInCash * 0.05)} $</Text>
-              </View>
-            </View>
-          </View>
-        ) : (
-          <View>
-            <Text style={styles.primaryText}>Total</Text>
-            <View style={styles.totalOrderInfoContainer}>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Order</Text>
-                <Text style={styles.secondaryTextSm}>10000000000$</Text>
-              </View>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Delivery</Text>
-                <Text style={styles.secondaryTextSm}>1000000000 $</Text>
-              </View>
-              <View style={styles.list}>
-                <Text style={styles.primaryTextSm}>Total In Coins</Text>
-                <Text style={styles.secondaryTextSm}>10000000000000 $</Text>
-              </View>
-            </View>
-          </View>
-        )}
-
-        <Text style={styles.primaryText}>Contact</Text>
-        <View style={styles.listContainer}>
-          <View style={styles.list}>
-            <Text style={styles.secondaryTextSm}>Email</Text>
-            <Text style={styles.secondaryTextSm}>mohamed@gmail.com</Text>
-          </View>
-          <View style={styles.list}>
-            <Text style={styles.secondaryTextSm}>Phone</Text>
-            <Text style={styles.secondaryTextSm}>+2001011627954</Text>
-          </View>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#C67C4E",
-              width: "80%",
-              paddingVertical: 10,
-              borderRadius: 10,
-              alignItems: "center",
-              marginTop: 50,
-              marginLeft: "10%",
-            }}
-            onPress={CheckOut} // add handleAuthentication
-          >
-            <Text
-              style={{
-                color: "white",
-                fontSize: 16,
-                fontFamily: "sora-regular",
-              }}
             >
               Order
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View></View>
         </View>
-        <View style={styles.emptyView}></View>
-      </ScrollView>
-    </View>
+        <ScrollView style={styles.bodyContainer} nestedScrollEnabled={true}>
+          <Text style={styles.primaryText}>Order Summary</Text>
+          <ScrollView
+            style={[styles.orderSummaryContainer]}
+            nestedScrollEnabled={true}
+          >
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={Products}
+              horizontal={false}
+              renderItem={(itemData) => {
+                return (
+                  <BasicProductList
+                    productName={itemData.item.productName}
+                    price={itemData.item.price}
+                    image={itemData.item.image}
+                    id={itemData.item.id}
+                  />
+                );
+              }}
+            />
+          </ScrollView>
+          <View style={{ marginTop: 20, flexDirection: "row" }}>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 14,
+                width: 153.5,
+                height: 40,
+                borderRadius: 10,
+                gap: 10,
+                flex: 0,
+                flexGrow: 1,
+                backgroundColor: index === "Cash" ? "#C67C4E" : "#F2F2F2",
+              }}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    width: 59,
+                    height: 20,
+                    fontFamily: "sora-regular",
+                    fontStyle: "normal",
+                    fontWeight: "600",
+                    fontSize: 16,
+                    lineHeight: 20,
+                    color: index === "Cash" ? "white" : "black",
+                    flex: 0,
+                    flexGrow: 0,
+                  }}
+                  onPress={() => setIndex("Cash")}
+                >
+                  Cash
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 14,
+                gap: 10,
+                width: 153.5,
+                height: 40,
+                backgroundColor: index === "Coins" ? "#C67C4E" : "#F2F2F2",
+                borderRadius: 10,
+                flex: 0,
+                flexGrow: 1,
+              }}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    width: 62,
+                    height: 20,
+                    fontFamily: "sora-regular",
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    fontSize: 16,
+                    lineHeight: 20,
+                    color: index === "Coins" ? "white" : "black",
+                    flex: 0,
+                    marginLeft: 25,
+                    flexGrow: 0,
+                  }}
+                  onPress={() => setIndex("Coins")}
+                >
+                  Coins
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {index === "Cash" ? (
+            <View>
+              <Text style={styles.primaryText}>Total</Text>
+              <View style={styles.totalOrderInfoContainer}>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Order</Text>
+                  <Text style={styles.secondaryTextSm}>{totalInCash}$</Text>
+                </View>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Delivery</Text>
+                  <Text style={styles.secondaryTextSm}>
+                    {totalInCash * 0.05} $
+                  </Text>
+                </View>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Total In Cash</Text>
+                  <Text style={styles.secondaryTextSm}>
+                    {totalInCash + totalInCash * 0.05} $
+                  </Text>
+                </View>
+              </View>
+            </View>
+          ) : (
+            <View>
+              <Text style={styles.primaryText}>Total</Text>
+              <View style={styles.totalOrderInfoContainer}>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Order</Text>
+                  <Text style={styles.secondaryTextSm}>10000000000$</Text>
+                </View>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Delivery</Text>
+                  <Text style={styles.secondaryTextSm}>1000000000 $</Text>
+                </View>
+                <View style={styles.list}>
+                  <Text style={styles.primaryTextSm}>Total In Coins</Text>
+                  <Text style={styles.secondaryTextSm}>10000000000000 $</Text>
+                </View>
+              </View>
+            </View>
+          )}
+
+          <Text style={styles.primaryText}>Contact</Text>
+          <View style={styles.listContainer}>
+            <View style={styles.list}>
+              <Text style={styles.secondaryTextSm}>Email</Text>
+              <Text style={styles.secondaryTextSm}>{email}</Text>
+            </View>
+            <View style={styles.list}>
+              <Text style={styles.secondaryTextSm}>Phone</Text>
+              <Text style={styles.secondaryTextSm}>{phone}</Text>
+            </View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#C67C4E",
+                width: "80%",
+                paddingVertical: 10,
+                borderRadius: 10,
+                alignItems: "center",
+                marginTop: 50,
+                marginLeft: "10%",
+              }}
+              onPress={handleAuthentication} // add handleAuthentication
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 16,
+                  fontFamily: "sora-regular",
+                }}
+              >
+                Order
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.emptyView}></View>
+        </ScrollView>
+      </View>
     </>
   );
 };
