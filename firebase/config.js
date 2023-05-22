bbb792c9247e90ae04a94ef482afb402f3a51b70
ShehabM0 <shehabmohamed2357@gmail.com>
2023-05-22 23:31:52 +0300

    repush config.js

---
+++ firebase/config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import {
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_MEASUREMENT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_API_KEY,
+    FIREBASE_APP_ID
+} from "@env";
+
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+export { app, auth , db , storage };
